9db3931ae02f03760285aeb2a611679f5e9d3cad
IT22171856 <142132191+IT22171856@users.noreply.github.com>
2025-02-07 07:28:12 +0530

    optimized code colors add to const

---
+++ bmi_cal/lib/constant.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const ktheme=[Color(0xFFFFB6C1), Color(0xFFADD8E6)];
--- bmi_cal/lib/main_page.dart
+++ bmi_cal/lib/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_cal/constant.dart';
 import 'package:flutter/material.dart';
 
 class MainApp extends StatefulWidget {
@@ -8,15 +9,18 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  int height = 150;
+  int weight = 70;
+  late double bmi = calculate(height: height, weight: weight);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar:AppBar(
+        appBar: AppBar(
           title: const Text('BMI Calculator'),
           flexibleSpace: Container(
             decoration: const BoxDecoration(
               gradient: LinearGradient(
-                colors: [Color(0xFFFFB6C1), Color(0xFFADD8E6)],
+                colors: ktheme,
                 begin: Alignment.centerLeft,
                 end: Alignment.centerRight,
               ),
@@ -29,14 +33,14 @@ class _MainAppState extends State<MainApp> {
             padding: const EdgeInsets.all(16.0),
             child: Column(
               children: [
-                Row(
-                 children:[
+                const Row(
+                  children: [
                     Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child:Center(
+                      padding: EdgeInsets.all(20.0),
+                      child: Center(
                         child: Column(
                           children: [
-                           Text("Male"),
+                            Text("Male"),
                             Icon(Icons.male,
                                 size: 150, color: Colors.lightBlueAccent),
                           ],
@@ -47,7 +51,7 @@ class _MainAppState extends State<MainApp> {
                     Padding(
                       padding: EdgeInsets.all(20.0),
                       child: Column(
-                        children:  [
+                        children: [
                           Text("Female"),
                           Icon(Icons.female,
                               size: 150, color: Colors.pinkAccent),
@@ -59,28 +63,45 @@ class _MainAppState extends State<MainApp> {
                 Row(
                   children: [
                     Padding(
-                      padding:EdgeInsets.all(20.0),
+                      padding: const EdgeInsets.all(20.0),
                       child: Center(
                         child: Column(
                           children: [
-                            const Text("Height"),
+                            const Text("Height(cm)"),
                             Text(
-                              "170",
-                              style: TextStyle(
+                              "$height",
+                              style: const TextStyle(
                                   fontSize: 64, fontWeight: FontWeight.bold),
                             ),
                             Row(
                               children: [
                                 FloatingActionButton(
-                                  onPressed: null,
-                                  child: Icon(Icons.add),
+                                  onPressed: () {
+                                    setState(() {
+                                      if (height >= 50) {
+                                        height--;
+                                        bmi = calculate(
+                                            height: height, weight: weight);
+                                      }
+                                    });
+                                    print(height);
+                                  },
+                                  child:const Icon(Icons.remove),
                                 ),
-                                SizedBox(
+                                const SizedBox(
                                   width: 15,
                                 ),
                                 FloatingActionButton(
-                                  onPressed: null,
-                                  child: Icon(Icons.remove),
+                                  onPressed: () {
+                                    setState(() {
+                                      if (height <= 300) {
+                                        height++;
+                                        bmi = calculate(
+                                            height: height, weight: weight);
+                                      }
+                                    });
+                                  },
+                                  child:const Icon(Icons.add),
                                 )
                               ],
                             )
@@ -88,29 +109,45 @@ class _MainAppState extends State<MainApp> {
                         ),
                       ),
                     ),
-                     Spacer(),
+                    const Spacer(),
                     Padding(
-                      padding:  EdgeInsets.all(20.0),
+                      padding: const EdgeInsets.all(20.0),
                       child: Column(
                         children: [
-                          Text("Width"),
+                          const Text("Weight(Kg)"),
                           Text(
-                            "70",
-                            style: TextStyle(
+                            "$weight",
+                            style: const TextStyle(
                                 fontSize: 64, fontWeight: FontWeight.bold),
                           ),
                           Row(
                             children: [
                               FloatingActionButton(
-                                onPressed: null,
-                                child: Icon(Icons.add),
+                                onPressed: () {
+                                  setState(() {
+                                    if (weight >= 10) {
+                                      weight--;
+                                      bmi = calculate(
+                                          height: height, weight: weight);
+                                    }
+                                  });
+                                },
+                                child: const Icon(Icons.remove),
                               ),
-                              SizedBox(
+                              const SizedBox(
                                 width: 15,
                               ),
                               FloatingActionButton(
-                                onPressed: null,
-                                child: Icon(Icons.remove),
+                                onPressed: () {
+                                  setState(() {
+                                    if (weight <= 500) {
+                                      weight++;
+                                      bmi = calculate(
+                                          height: height, weight: weight);
+                                    }
+                                  });
+                                },
+                                child:const Icon(Icons.add),
                               )
                             ],
                           )
@@ -119,7 +156,7 @@ class _MainAppState extends State<MainApp> {
                     )
                   ],
                 ),
-                Text(
+                const Text(
                   "BMI",
                   style: TextStyle(
                       color: Colors.grey,
@@ -130,24 +167,38 @@ class _MainAppState extends State<MainApp> {
                     width: 200,
                     height: 100,
                     decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Color(0xFFFFB6C1),
-                            Color(0xFFADD8E6)
-                          ], // Updated gradient colors
+                        gradient: const LinearGradient(
+                          colors: ktheme, // Updated gradient colors
                           begin: Alignment.topLeft,
                           end: Alignment.bottomRight,
                         ),
                         borderRadius: BorderRadius.circular(20)),
                     child: Center(
                         child: Text(
-                      "20",
+                      "${bmi.toStringAsFixed(2)}",
                       style:
-                          TextStyle(fontSize: 64, fontWeight: FontWeight.bold),
+                          const TextStyle(fontSize: 64, fontWeight: FontWeight.bold),
                     ))),
+                Text(result(bmi))
               ],
             ),
           ),
         ));
+  }
+
+  double calculate({required int height, required int weight}) {
+    return (weight / (height * height)) * 10000;
+  }
+
+  String result(double bmi) {
+    if (bmi < 18.5) {
+      return 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      return "Normal weight";
+    } else if (bmi >= 25 && bmi < 29.9) {
+      return "Overweight";
+    } else {
+      return "Obesity";
+    }
   }
 }
